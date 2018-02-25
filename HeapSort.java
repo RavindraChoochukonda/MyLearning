9db4297f34779a69e17220abbf04b2f22fc051ee
RavindraChoochukonda <36820155+RavindraChoochukonda@users.noreply.github.com>
2018-02-25 22:14:46 +0530

    Add files via upload

---
+++ HeapSort.java
@@ -0,0 +1,41 @@
+package com.pursuit.sorting;
+
+public class HeapSort {
+    public void sort(int[] a){
+        int n = a.length;
+
+        for(int i=n/2-1;i>=0;i--){
+            heapify(a,n,i);
+        }
+
+        for(int i=n-1;i>0;i--){
+            int swap = a[0];
+            a[0] = a[i];
+            a[i] = swap;
+
+            heapify(a,i,0);
+        }
+
+    }
+
+    public void heapify(int[] a,int n,int i){
+        int max = i;
+        int left = 2*i+1;
+        int right = 2*i+2;
+
+        if(left<n && a[left]>a[max]){
+            max=left;
+        }
+        if(right<n && a[right]>a[max]){
+            max = right;
+        }
+
+        if(max!=i){
+            int swap = a[i];
+            a[i]=a[max];
+            a[max] = swap;
+
+            heapify(a,n,max);
+        }
+    }
+}
